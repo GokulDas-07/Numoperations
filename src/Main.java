5a7ee9eeb9138b8e0824de26c7c227e0b5547d46
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-11-21 12:49:15 +0530

    prime or not added

--- src/Main.java
+++ src/Main.java
@@ -17,20 +17,52 @@ public class Main {
             case 1:
                 if(num1>num2)
                 {
-                    System.out.println(num1 +" is largest");
+                    System.out.println(num1 +" is largest.");
                 }else
                 {
-                    System.out.println(num2 +" is largest");
+                    System.out.println(num2 +" is largest.");
                 }
 
             case 2:
                 if(num1>num2)
                 {
-                    System.out.println(num2 +" is Smallest");
+                    System.out.println(num2 +" is Smallest.");
                 }else
                 {
-                    System.out.println(num1 +" is Smallest");
+                    System.out.println(num1 +" is Smallest.");
                 }
+
+            case 3:
+                boolean flag = false;
+                // prime or not for num1
+                for (int i = 2; i <= num1 / 2; ++i)
+                {
+                    if (num1 % i == 0)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag)
+                    System.out.println(num1 + " is a prime number.");
+                else
+                    System.out.println(num1 + " is not a prime number.");
+
+                //prime or not for num2
+                for (int i = 2; i <= num2 / 2; ++i)
+                {
+                    if (num2 % i == 0)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag)
+                    System.out.println(num2 + " is a prime number.");
+                else
+                    System.out.println(num2 + " is not a prime number.");
+
+
         }
 
     }
